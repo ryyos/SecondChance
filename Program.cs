a8b690dbc965251023532bed9502b0883b04bc7c
Ryyos <riodwi12174@gmail.com>
2025-11-03 03:48:00 +0700

    feat: add more blueprint

--- Program.cs
+++ Program.cs
@@ -1,2 +1,47 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+using System.IO;
+using Serilog;
+
+public class Master
+{
+
+    private static string pathWelcome = "assets/txt/welcome.txt";
+    private static string pathLogs = "logs/log_.txt";
+    static Master()
+    {
+        Log.Logger = new LoggerConfiguration()
+            .WriteTo.Console()
+            .WriteTo.File(pathLogs, rollingInterval: RollingInterval.Day)
+            .CreateLogger();
+    }
+
+    public static void Continue()
+    {
+
+    }
+
+    public static void NewGame()
+    {
+
+    }
+
+    public static void Dashboard()
+    {
+
+    }
+
+    public static void Main(string[] args)
+    {
+        Console.WriteLine(
+            File.ReadAllText(pathWelcome)
+        );
+
+        Console.Write("Select an option: ");
+        bool status = int.TryParse(Console.ReadLine()!, out int chose);
+        if (!status)
+        {
+            Log.Error("input not valid");
+        }
+        ;
+    }
+}
